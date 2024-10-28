46eb3ee8c673836c7306723bd91440b404ad91db
Daniel Barcelona <75101802+DanielBCNA@users.noreply.github.com>
2024-10-28 20:09:56 +0000

    Quartz sync: Oct 28, 2024, 8:09 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,12 +25,16 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+//    Component.Search()
+    Component.MobileOnly(Component.Search()),
+//    Component.Darkmode(),
+    Component.MobileOnly(Component.Darkmode()),
 //    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
 //    Component.Graph(),
+    Component.DesktopOnly(Component.Search()),
+    Component.DesktopOnly(Component.Darkmode()),
     Component.DesktopOnly(Component.TableOfContents()),
 //    Component.Backlinks(),
   ],
